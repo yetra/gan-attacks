9e432793d77f7b8f90c702cab96a2a00eead3f92
yetra <brdujmovic@gmail.com>
2021-12-30 18:42:44 +0100

    Add discriminator for MNIST

--- dcgan.py
+++ dcgan.py
@@ -41,3 +41,21 @@ def mnist_generator():
     assert model.output_shape == (None, 28, 28, 1)
 
     return model
+
+
+def mnist_discriminator():
+    model = keras.Sequential(name='mnist_discriminator')
+    model.add(layers.Conv2D(
+        64, (5, 5), strides=(2, 2), padding='same', input_shape=[28, 28, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(
+        128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
