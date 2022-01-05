d769d937b2d9b365c926e14da663552ea5821425
yetra <brdujmovic@gmail.com>
2022-01-05 23:13:58 +0100

    Remove the functions for builing MNIST DCGAN components

--- dcgan.py
+++ dcgan.py
@@ -1,51 +1,7 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-
 import matplotlib.pyplot as plt
 
-
-def mnist_generator():
-    model = keras.Sequential(name='mnist_generator')
-
-    model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(100,)))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-
-    model.add(layers.Reshape((7, 7, 256)))
-
-    model.add(layers.Conv2DTranspose(
-        128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-
-    model.add(layers.Conv2DTranspose(
-        64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-
-    model.add(layers.Conv2DTranspose(
-        1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-
-    return model
-
-
-def mnist_discriminator():
-    model = keras.Sequential(name='mnist_discriminator')
-    model.add(layers.Conv2D(
-        64, (5, 5), strides=(2, 2), padding='same', input_shape=[28, 28, 1]))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
-
-    model.add(layers.Conv2D(
-        128, (5, 5), strides=(2, 2), padding='same'))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
-
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1, activation='sigmoid'))
-
-    return model
+import tensorflow as tf
+from tensorflow import keras
 
 
 class DCGAN(keras.Model):
