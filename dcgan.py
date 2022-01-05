d2ee5b319ed74b850b852aac39afb33ed08a6cb7
yetra <brdujmovic@gmail.com>
2022-01-05 23:42:33 +0100

    Move GANMonitor to util

--- dcgan.py
+++ dcgan.py
@@ -1,5 +1,3 @@
-import matplotlib.pyplot as plt
-
 import tensorflow as tf
 from tensorflow import keras
 
@@ -73,21 +71,3 @@ class DCGAN(keras.Model):
             'd_loss': self.d_loss_metric.result(),
             'g_loss': self.g_loss_metric.result(),
         }
-
-
-class GANMonitor(keras.callbacks.Callback):
-    def __init__(self, num_images, latent_dim):
-        super().__init__()
-
-        self.noise = tf.random.normal(shape=(num_images, latent_dim))
-
-    def on_epoch_end(self, epoch, logs=None):
-        predictions = self.model.generator(self.noise, training=False)
-
-        for i in range(predictions.shape[0]):
-            plt.subplot(4, 4, i + 1)
-            plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
-            plt.axis('off')
-
-        plt.savefig(f'epoch_{epoch + 1:03d}.png')
-        plt.show()
