c7cf8a43306ec3313f3e3d83e98ce8ee1eac6597
yetra <brdujmovic@gmail.com>
2022-01-05 23:51:31 +0100

    Add a function for creating a GIF from saved images

--- util.py
+++ util.py
@@ -1,10 +1,11 @@
+import glob
+
+import imageio
 import matplotlib.pyplot as plt
-
 import tensorflow as tf
-from tensorflow import keras
 
 
-class GANMonitor(keras.callbacks.Callback):
+class GANMonitor(tf.keras.callbacks.Callback):
     def __init__(self, num_images, latent_dim):
         super().__init__()
 
@@ -20,3 +21,22 @@ class GANMonitor(keras.callbacks.Callback):
 
         plt.savefig(f'epoch_{epoch + 1:03d}.png')
         plt.show()
+
+
+def create_gif(image_pattern, gif_path):
+    """
+    Creates a GIF from the images located at `image_pattern`.
+
+    :param image_pattern: filename pattern of the images (GIF frames)
+    :param gif_path: path to the GIF
+    """
+    with imageio.get_writer(gif_path, mode='I') as writer:
+        filenames = glob.glob(image_pattern)
+        filenames = sorted(filenames)
+
+        for filename in filenames:
+            image = imageio.imread(filename)
+            writer.append_data(image)
+
+        image = imageio.imread(filename)
+        writer.append_data(image)
