f39d92e669d4a5cb9d8fc3210a1065979738114a
yetra <brdujmovic@gmail.com>
2022-01-05 23:13:21 +0100

    Remove load_mnist

--- dcgan.py
+++ dcgan.py
@@ -5,17 +5,6 @@ from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 
 
-def load_mnist(buffer_size, batch_size, input_shape=(28, 28, 1)):
-    (images, labels), (_, _) = keras.datasets.mnist.load_data()
-
-    images = images.reshape(images.shape[0], *input_shape).astype('float32')
-    images = (images - 127.5) / 127.5  # normalize images to [-1, 1]
-
-    return tf.data.Dataset.from_tensor_slices(images)\
-        .shuffle(buffer_size)\
-        .batch(batch_size)
-
-
 def mnist_generator():
     model = keras.Sequential(name='mnist_generator')
 
