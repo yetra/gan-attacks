3b6f1b4d2fa59903c306e91efb72ceaf788d3a5f
yetra <brdujmovic@gmail.com>
2022-01-12 23:23:19 +0100

    Implement train_step for AdvGAN

--- advgan.py
+++ advgan.py
@@ -32,3 +32,38 @@ class AdvGAN(tf.keras.Model):
         fake_loss = self.loss_fn(tf.zeros_like(fake_output), fake_output)
 
         return (real_loss + fake_loss) * 0.5
+
+    def train_step(self, real_images):
+        # train the discriminator and the generator (separately)
+        with tf.GradientTape() as d_tape, tf.GradientTape() as g_tape:
+            perturbations = self.generator(real_images, training=True)
+            # perturbations = tf.clip_by_value(perturbations, -0.3, 0.3)
+            adv_images = tf.clip_by_value(real_images + perturbations, -1, 1)
+
+            target_output = self.target(adv_images)
+            adv_loss = self.adv_loss_fn(target_output)
+
+            perturb_loss = self.perturb_loss_fn(perturbations)
+
+            real_output = self.discriminator(real_images, training=True)
+            fake_output = self.discriminator(adv_images, training=True)
+
+            d_loss = self.discriminator_loss(real_output, fake_output)
+
+            g_gan_loss = self.generator_loss(fake_output)
+            g_loss = adv_loss + g_gan_loss * self.lambda_gan + perturb_loss * self.lambda_perturb
+
+        # calculate the gradients for the generators and discriminators
+        d_grads = d_tape.gradient(d_loss, self.discriminator.trainable_variables)
+        g_grads = g_tape.gradient(g_loss, self.generator.trainable_variables)
+
+        # apply the gradients to the optimizers
+        self.d_optimizer.apply_gradients(
+            zip(d_grads, self.discriminator.trainable_variables))
+        self.g_optimizer.apply_gradients(
+            zip(g_grads, self.generator.trainable_variables))
+
+        return {
+            'd_loss': d_loss,
+            'g_loss': g_loss,
+        }
