416f05defd931115919f10a0b1e462a2dd83cb62
yetra <brdujmovic@gmail.com>
2022-01-12 23:22:48 +0100

    Add generator and discriminator loss functions for AdvGAN

--- advgan.py
+++ advgan.py
@@ -23,3 +23,12 @@ class AdvGAN(tf.keras.Model):
 
         self.lambda_gan = lambda_gan
         self.lambda_perturb = lambda_perturb
+
+    def generator_loss(self, fake_output):
+        return self.loss_fn(tf.ones_like(fake_output), fake_output)
+
+    def discriminator_loss(self, real_output, fake_output):
+        real_loss = self.loss_fn(tf.ones_like(real_output), real_output)
+        fake_loss = self.loss_fn(tf.zeros_like(fake_output), fake_output)
+
+        return (real_loss + fake_loss) * 0.5
