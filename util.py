c6542597f77842c5947c0096518e6e2844799f8c
yetra <brdujmovic@gmail.com>
2022-05-31 17:37:05 +0200

    Remove GANMonitor from util

--- util.py
+++ util.py
@@ -12,37 +12,6 @@ import tensorflow as tf
 from IPython.display import display, Audio
 
 
-class GANMonitor(tf.keras.callbacks.Callback):
-    """
-    A callback for monitoring a GAN's progress. On epoch end, the generator
-    is used to create a specified number of images, which are then saved to
-    the specified location.
-    """
-    def __init__(self, num_images, latent_dim, path_prefix):
-        """
-        Inits the `GANMonitor`.
-
-        :param num_images: the number of images to generate
-        :param latent_dim: the size of the noise vector
-        :param path_prefix: prefix of the path at which to save the images
-        """
-        super().__init__()
-
-        self.path_prefix = path_prefix
-        self.noise = tf.random.normal(shape=(num_images, latent_dim))
-
-    def on_epoch_end(self, epoch, logs=None):
-        predictions = self.model.generator(self.noise, training=False)
-
-        for i in range(predictions.shape[0]):
-            plt.subplot(4, 4, i + 1)
-            plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
-            plt.axis('off')
-
-        plt.savefig(f'{self.path_prefix}_{epoch + 1:03d}.png')
-        plt.show()
-
-
 class ImageAdvGANCallback(tf.keras.callbacks.Callback):
     """
     A callback for creating and saving a specified number of adversarial images
