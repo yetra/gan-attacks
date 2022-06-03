626d9946e78fb7560e8cdccf9ada58a64260c442
yetra <brdujmovic@gmail.com>
2022-06-03 10:51:26 +0200

    Reshape perturbations before tf.norm

--- losses.py
+++ losses.py
@@ -13,6 +13,8 @@ def carlini_wagner_loss_fn(target_label):
 
 def l2_norm_soft_hinge_loss_fn(bound):
     def _l2_norm_soft_hinge_loss_fn(perturbations):
-        return tf.reduce_mean(tf.maximum(tf.norm(perturbations, axis=1) - bound, 0))
+        reshaped = tf.reshape(perturbations, (perturbations.shape[0], -1))
+
+        return tf.reduce_mean(tf.maximum(tf.norm(reshaped, axis=1) - bound, 0))
 
     return _l2_norm_soft_hinge_loss_fn
